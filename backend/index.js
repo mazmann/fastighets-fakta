0d5e320afc3f7e777bcf325404c1a6d037eb546c
mazmann <sadi.lundberg@hotmail.com>
2024-01-11 17:10:41 +0100

    inputfields

--- backend/index.js
+++ backend/index.js
@@ -6,35 +6,67 @@ const app = express();
 const port = 5000;
 
 mongoose.connect('mongodb://localhost:27017/', {
-  dbName: 'fastighets_info',
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    dbName: 'fastighets_info',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-  .then(() => {
-    console.log('Connected to yourDB-name database');
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
+    .then(() => {
+        console.log('Connected to yourDB-name database');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
 
 // Schema for users of app
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+    propertyOwner: {
+        type: String,
+        required: true,
+    },
+    organisationNumber: {
+        type: String,
+        required: true,
+    },
+    properyTag: {
+        type: String,
+        required: true,
+    },
+    propertyAdress: {
+        type: String,
+        required: true,
+    },
+    propertyArea: {
+        type: String,
+        required: true,
+    },
+    visitingAdress: {
+        type: String,
+        required: true,
+    },
+    visitingArea: {
+        type: String,
+        required: true,
+    },
+    contactRep: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
-const User = mongoose.model('users', UserSchema);
+const User = mongoose.model('hummer', UserSchema);
 
 // For backend and express
 app.use(express.json());
@@ -43,29 +75,29 @@ app.use(cors());
 app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.get('/', (req, resp) => {
-  resp.send('App is Working');
-  // You can check if the backend is working or not by entering http://localhost:5000
-  // If you see "App is working," the backend is working properly.
+    resp.send('App is Working');
+    // You can check if the backend is working or not by entering http://localhost:5000
+    // If you see "App is working," the backend is working properly.
 });
 
 app.post('/register', async (req, resp) => {
-  try {
-    const user = new User(req.body);
-    let result = await user.save();
-    result = result.toObject();
-    if (result) {
-      delete result.password; // Assuming there's a password field that you want to exclude
-      resp.json(result);
-      console.log(result);
-    } else {
-      console.log('User already registered');
+    try {
+        const user = new User(req.body);
+        let result = await user.save();
+        result = result.toObject();
+        if (result) {
+            delete result.password; // Assuming there's a password field that you want to exclude
+            resp.json(result);
+            console.log(result);
+        } else {
+            console.log('User already registered');
+        }
+    } catch (e) {
+        console.error('Something Went Wrong:', e);
+        resp.status(500).send('Something Went Wrong');
     }
-  } catch (e) {
-    console.error('Something Went Wrong:', e);
-    resp.status(500).send('Something Went Wrong');
-  }
 });
 
 app.listen(port, () => {
-  console.log(`App listen at port ${port}`);
+    console.log(`App listen at port ${port}`);
 });
--- src/App.js
+++ src/App.js
@@ -4,51 +4,84 @@ import { useState } from 'react'
 import FiveInputFieldsComponent from './components/inputFields';
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const handleOnSubmit = async (e) => {
-      e.preventDefault();
-      let result = await fetch(
-      'http://localhost:5000/register', {
-          method: "post",
-          body: JSON.stringify({ name, email }),
-          headers: {
-              'Content-Type': 'application/json'
-          }
-      })
-      result = await result.json();
-      console.warn(result);
-      if (result) {
-          alert("Data saved succesfully");
-          setEmail("");
-          setName("");
-      }
-  }
+    const [propertyOwner, setPropertyOwner] = useState("");
+    const [organisationNumber, setOrganisationNumber] = useState("");
+    const [properyTag, setProperyTag] = useState("");
+    const [propertyAdress, PropertyAdress] = useState("");
+    const [propertyArea, setPropertyArea] = useState("");
+    const [visitingAdress, setVisitingAdress] = useState("");
+    const [visitingArea, setVisitingArea] = useState("");
+    const [contactRep, setContactRep] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [email, setEmail] = useState("");
+    const [date, setDate] = useState("");
+    const handleOnSubmit = async (e) => {
+        e.preventDefault();
+        let result = await fetch(
+            'http://localhost:5000/register', {
+            method: "post",
+            body: JSON.stringify({
+                propertyOwner,
+                organisationNumber,
+                properyTag,
+                propertyAdress,
+                propertyArea,
+                visitingAdress,
+                visitingArea,
+                contactRep,
+                phoneNumber,
+                email,
+                date
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        result = await result.json();
+        console.warn(result);
+        if (result) {
+            alert("Data saved succesfully");
+            setEmail("");
+        
+        }
+    }
+    return (
+
+        <>
+            <div className="App">
+            <h1>This is React WebApp </h1>
+            <form action="">
+                <input type="text" placeholder="name"
+                    value={propertyOwner} onChange={(e) => setPropertyOwner(e.target.value)} />
+                <input type="text" placeholder="name"
+                    value={organisationNumber} onChange={(e) => setOrganisationNumber(e.target.value)} />
+                <input type="text" placeholder="name"
+                    value={properyTag} onChange={(e) => setProperyTag(e.target.value)} />
+                <input type="text" placeholder="name"
+                    value={propertyAdress} onChange={(e) => PropertyAdress(e.target.value)} />
+                <input type="text" placeholder="name"
+                    value={propertyArea} onChange={(e) => setPropertyArea(e.target.value)} />
+                <input type="text" placeholder="name"
+                    value={visitingAdress} onChange={(e) => setVisitingAdress(e.target.value)} />
+                <input type="text" placeholder="name"
+                    value={visitingArea} onChange={(e) => setVisitingArea(e.target.value)} />
+                <input type="text" placeholder="name"
+                    value={contactRep} onChange={(e) => setContactRep(e.target.value)} />
+                <input type="text" placeholder="name"
+                    value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+                <input type="email" placeholder="email"
+                    value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="text" placeholder="name"
+                    value={date} onChange={(e) => setDate(e.target.value)} />
+                <button type="submit"
+                    onClick={handleOnSubmit}>submit</button>
+            </form>
+            </div>
+        </>
 
 
-  return (
 
-    <>
-   <div className="App">
-      <h1>Hej</h1>
-      <FiveInputFieldsComponent />
-    </div>
-
-    <h1>This is React WebApp </h1>
-    <form action="">
-        <input type="text" placeholder="name"
-        value={name} onChange={(e) => setName(e.target.value)} />
-        <input type="email" placeholder="email"
-        value={email} onChange={(e) => setEmail(e.target.value)} />
-        <button type="submit"
-        onClick={handleOnSubmit}>submit</button>
-    </form>
-
-</>
-
-
-
-  );
+    );
 }
 
 export default App;
