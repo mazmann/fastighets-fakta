432b928f94e181ddcbbfd2a5d5786539992f9363
mazmann <sadi.lundberg@hotmail.com>
2024-01-14 14:10:19 +0100

    started in prop details

--- src/components/PropertyData.jsx
+++ src/components/PropertyData.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { FetchFurtherData } from '../api/FetchFurtherData';
+import './PropertyData.css'
 
 const PropertyData = () => {
   const { propertyId } = useParams();
@@ -21,15 +22,91 @@ const PropertyData = () => {
   }, [propertyId]);
 
   return (
-    <div>
+    <div className='App-display-more'>
+     <h2>Property Details</h2>
       {property ? (
-        <div>
-          <h2>Property Details</h2>
-          <p>Contact Representative: {property.propertyAddress}</p>
-          <p>Date: {property.date}</p>
-          <p>Email: {property.email}</p>
-          {/* Add more properties as needed */}
-        </div>
+        <form>
+ <table>
+    <tbody>
+      <tr>
+        <td>
+          <label>Property Owner:</label>
+        </td>
+        <td>
+          <label>Organisation Number:</label>
+        </td>
+        <td>
+          <label>Property Tag:</label>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <p>{property.propertyOwner}</p>
+        </td>
+        <td>
+          <p>{property.organisationNumber}</p>
+        </td>
+        <td>
+          <p>{property.propertyTag}</p>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <label>Property Address:</label>
+        </td>
+        <td>
+          <label>Property Area:</label>
+        </td>
+        <td>
+          <label>Visiting Address:</label>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <p>{property.propertyAddress}</p>
+        </td>
+        <td>
+          <p>{property.propertyArea}</p>
+        </td>
+        <td>
+          <p>{property.visitingAddress}</p>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <label>Visiting Area:</label>
+        </td>
+        <td>
+          <label>Contact Representative:</label>
+        </td>
+        <td>
+          <label>Phone Number:</label>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <p>{property.visitingArea}</p>
+        </td>
+        <td>
+          <p>{property.contactRep}</p>
+        </td>
+        <td>
+          <p>{property.phoneNumber}</p>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <label>Email:</label>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <p>{property.email}</p>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+        </form>
       ) : (
         <p>Loading...</p>
       )}
