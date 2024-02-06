1d104506fab294ef66cd3430872c61bb1b5143a2
mazmann <sadi.lundberg@hotmail.com>
2024-02-06 13:15:27 +0100

    sorted css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import DisplayProperties from './components/DisplayProperties';
 import PropertyData from './components/PropertyData';
 import Navigation from './components/Navigation';
 import EditProperty from './components/EditProperty';
-import PropertyOwner from './components/PropertyOwner'
+import PropertyOwner from './components/PropertyOwner';
+import Home from './components/Home';
 
 
 
@@ -16,7 +17,7 @@ function App() {
     <>
     <Navigation />
       <Routes>
-        <Route path="/" element={<NewProperty />} />
+        <Route path="/" element={<Home />} />
         <Route path="/display" element={<DisplayProperties />} />
         <Route path="/property/:propertyId" element={<PropertyData />} />
         <Route path="/edit/:propertyId" element={<EditProperty />} />
