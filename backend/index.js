c5f47f81ab1c6bfc3b1ec0208064b4149dffd6c4
mazmann <sadi.lundberg@hotmail.com>
2024-03-03 18:09:06 +0100

    changed new property

--- backend/index.js
+++ backend/index.js
@@ -69,6 +69,9 @@ const insertedProperty = new property({
         visitingArea: "",
         contactRep: "Bengt Mikael Nordahl",
         phoneNumber: "087957801",
+        email: "",
+        date: "",
+        secondDate: "",
 });
 
 await insertedProperty.save();
